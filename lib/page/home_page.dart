7656fa87dd15a8e40a45551e101072d4df5ebd8d
Juwon-Yun <eooll2@daum.net>
2022-05-02 16:26:40 +0900

    feat: text style

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -11,8 +11,9 @@ class JuwonHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       // flutter is not recommend new keyword
-      // 자식이 없다면 전체 화면을 먹는다.
+      // container color는 자식이 없다면 전체 화면을 먹는다.
       // 있다면 width 만큼 먹는다.
+      // Center위젯은 옵션이 별로없어서 주로 container를 사용한다.
       body: Container(color: Colors.yellow,
           // padding: const EdgeInsets.all(10),
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
@@ -21,7 +22,16 @@ class JuwonHomePage extends StatelessWidget {
           // alignment: Alignment.topCenter,
           // x = 0, y = 0이 center이다.
           alignment: const Alignment(0, -1),
-          child: const Text("flutter juwon ")),
+          width: 200,
+          height: 300,
+          // SizedBox를 감싼 자식을
+          child: const SizedBox(
+            // width: 200,
+           // height: 200,
+           child : Text("flutter juwon ", style: TextStyle(fontSize: 20),
+           ),
+          ),
+          )
     );
   }
 }
